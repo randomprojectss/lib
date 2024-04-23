9255742b78bafa98789322b4c11774a545c1dfad
VortexHubScripts <167828839+VortexHubScripts@users.noreply.github.com>
2024-04-23 01:19:51 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3519,7 +3519,7 @@ function Library:CreateWindow(...)
 
         if Toggled then
             -- A bit scuffed, but if we're going from not toggled -> toggled we want to show the frame immediately so that the fade is visible.
-            Outer.Visible = true;
+            Outer.Visible = false;
 
             task.spawn(function()
                 -- TODO: add cursor fade?
@@ -3527,14 +3527,14 @@ function Library:CreateWindow(...)
 
                 local Cursor = Drawing.new('Triangle');
                 Cursor.Thickness = 1;
-                Cursor.Filled = true;
+                Cursor.Filled = false;
                 Cursor.Visible = false;
 
                 local CursorOutline = Drawing.new('Triangle');
                 CursorOutline.Thickness = 1;
                 CursorOutline.Filled = false;
                 CursorOutline.Color = Color3.new(0, 0, 0);
-                CursorOutline.Visible = true;
+                CursorOutline.Visible = false;
 
                 while Toggled and ScreenGui.Parent do
                     InputService.MouseIconEnabled = false;
