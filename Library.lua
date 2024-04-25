9b227656762de3f9419efc1c7e946f761544ebb3
VortexHubScripts <167828839+VortexHubScripts@users.noreply.github.com>
2024-04-25 12:02:32 -0400

    Update Library1.lua

--- Library.lua
+++ Library.lua
@@ -518,7 +518,7 @@ do
             AnchorPoint = Vector2.new(0.5, 0.5);
             Size = UDim2.new(0, 6, 0, 6);
             BackgroundTransparency = 1;
-            Image = 'http://www.roblox.com/asset/?id=9619665977';
+            Image = '';
             ImageColor3 = Color3.new(0, 0, 0);
             ZIndex = 19;
             Parent = SatVibMap;
@@ -528,7 +528,7 @@ do
             Size = UDim2.new(0, CursorOuter.Size.X.Offset - 2, 0, CursorOuter.Size.Y.Offset - 2);
             Position = UDim2.new(0, 1, 0, 1);
             BackgroundTransparency = 1;
-            Image = 'http://www.roblox.com/asset/?id=9619665977';
+            Image = '';
             ZIndex = 20;
             Parent = CursorOuter;
         })
