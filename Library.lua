9bd47631b07bcbaeb3f5c1d6bea7835e43c2b989
randomprojectss <santongxv@gmail.com>
2024-10-16 00:03:42 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -2720,23 +2720,7 @@ do
         Parent = Library.NotificationArea;
     });
 
-    local WatermarkOuter = Library:Create('Frame', {
-        BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 100, 0, -25);
-        Size = UDim2.new(0, 213, 0, 20);
-        ZIndex = 200;
-        Visible = false;
-        Parent = ScreenGui;
-    });
-
-    local WatermarkInner = Library:Create('Frame', {
-        BackgroundColor3 = Library.MainColor;
-        BorderColor3 = Library.AccentColor;
-        BorderMode = Enum.BorderMode.Inset;
-        Size = UDim2.new(1, 0, 1, 0);
-        ZIndex = 201;
-        Parent = WatermarkOuter;
-    });
+    
 
     Library:AddToRegistry(WatermarkInner, {
         BorderColor3 = 'AccentColor';
