e0b85db9faf9ab3955986d479cab56a6dfac713f
randomprojectss <santongxv@gmail.com>
2024-10-15 23:39:27 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3012,14 +3012,7 @@ function Library:CreateWindow(...)
         BorderColor3 = 'AccentColor';
     });
 
-    local WindowLabel = Library:CreateLabel({
-        Position = UDim2.new(0, 7, 0, 0);
-        Size = UDim2.new(0, 0, 0, 25);
-        Text = Config.Title or '';
-        TextXAlignment = Enum.TextXAlignment.Left;
-        ZIndex = 1;
-        Parent = Inner;
-    });
+   
 
     local MainSectionOuter = Library:Create('Frame', {
         BackgroundColor3 = Library.BackgroundColor;
