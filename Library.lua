518728ed78cdb131c2a32ef648df193394e6530a
randomprojectss <santongxv@gmail.com>
2024-10-16 05:42:49 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3032,10 +3032,7 @@ function Library:CreateWindow(...)
         Parent = Inner;
     });
 
-    Library:AddToRegistry(MainSectionOuter, {
-        BackgroundColor3 = 'BackgroundColor';
-        BorderColor3 = 'OutlineColor';
-    });
+    
 
     local MainSectionInner = Library:Create('Frame', {
         BackgroundColor3 = Library.BackgroundColor;
@@ -3047,10 +3044,7 @@ function Library:CreateWindow(...)
         Parent = MainSectionOuter;
     });
 
-    Library:AddToRegistry(MainSectionInner, {
-        BackgroundColor3 = 'BackgroundColor';
-    });
-
+    
 
     local TabArea = Library:Create('Frame', {
         BackgroundTransparency = 1;
@@ -3125,9 +3119,7 @@ function Library:CreateWindow(...)
             Parent = TabButton;
         });
 
-        Library:AddToRegistry(Blocker, {
-            BackgroundColor3 = 'MainColor';
-        });
+        
 
         local TabFrame = Library:Create('Frame', {
             Name = 'TabFrame',
